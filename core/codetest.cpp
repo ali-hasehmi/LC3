5b8d6304574a6ab28c836fe1d70951faae6410b1
arminssth <arminssth@gmail.com>
2024-07-05 21:50:34 +0330

    ADD and it's required functions were added! 4 new funcctions!

--- core/codetest.cpp
+++ core/codetest.cpp
@@ -6,6 +6,41 @@ void Fetch(uint16_t *Memory ,uint16_t &MAR ,uint16_t &IR ,uint16_t &PC){
     MAR = PC;
     IR = Memory[MAR], PC++;
 }
+int getNegNumber(int n){
+    int negnumber = (-1)*(32-n);
+    return negnumber;
+}
+int EXPANDimm5(uint16_t imm5){
+    if(imm5 >= 16) //number is negative
+    {   
+        int negnumber = getNegNumber(imm5);
+        return negnumber;
+    }
+    else{
+        int posnumber = imm5;
+        return posnumber;
+    }
+}
+void ADD(uint16_t &IR ,uint16_t *R){
+    uint16_t mask = 3584;
+    uint16_t DR = (IR & mask) / 512;
+    mask = 448;
+    uint16_t SR1 = (IR & mask) / 64;
+    mask = 32;
+    uint16_t flag = (IR & mask) / 32;
+    if(flag = 0){ // SR2 exists
+        mask = 7;
+        uint16_t SR2 = (IR & mask) / 1;
+        R[DR] = R[SR1] + R[SR2];
+    }
+    else { // No SR2. imm5 
+        mask = 31;
+        uint16_t imm5 = IR & mask;
+        int imm5_extended = EXPANDimm5(imm5);
+        R[DR] = R[SR1] + imm5_extended;
+    }
+
+}
 
 void Decode(uint16_t *Memory, uint16_t &PC, uint16_t &IR, uint16_t &MDR, uint16_t &MAR, uint16_t *R){
     uint16_t mask = 61440;
@@ -15,7 +50,7 @@ void Decode(uint16_t *Memory, uint16_t &PC, uint16_t &IR, uint16_t &MDR, uint16_
 
             break;
         case 4096://ADD & ADD
-            
+            ADD(IR ,R);
             break;
         case 8192://LD
 
