1279fbb7e397010418c5bfad88612cb472224c26
ali-hasehmi <AGHashemi.1382@gmail.com>
2024-07-07 02:07:21 +0330

    tokenize added + handleORGKW()

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -2,7 +2,25 @@
 #include <cstring>
 #include <vector>
 #include <string>
+#include <map>
+#include <cstdint>
 
+int handleORGKW(std::vector<std::string> &l)
+{
+    if (l.size() != 2)
+    {
+        return -1;
+    }
+    if (l[0] != "ORG")
+    {
+        return 1;
+    }
+    if (l[1] != "0x3000" && l[1] != "x3000")
+    {
+        return 2;
+    }
+    return 0;
+}
 void getRidOfComments(char *const s)
 {
     char *res = strchr(s, ';'); // find the location of the first ;
@@ -12,6 +30,24 @@ void getRidOfComments(char *const s)
     }
 }
 
+uint16_t binToUI16(const char *bin)
+{
+    uint16_t res = 0;
+    int j = 0;
+    for (int i = strlen(bin) - 1; i >= 0; --i)
+    {
+        if (bin[i] == '1')
+        {
+            res = res | 1 << j;
+        }
+        else
+        {
+            res = res | 0 << j;
+        }
+        ++j;
+    }
+    return res;
+}
 void getRidOfNewLine(char *const s)
 {
     char *res = strchr(s, '\n');
@@ -54,6 +90,7 @@ int main(int argc, char *argv[])
                 "error: missing file name after '-o'\n");
         exit(-1);
     }
+
     FILE *input_file = fopen(input_file_path, "r");
     if (input_file == NULL)
     {
@@ -61,6 +98,7 @@ int main(int argc, char *argv[])
                 "error: coudn't open %s\n", input_file_path);
         return -1;
     }
+
     FILE *output_file = fopen(output_file_path, "w");
     if (output_file == NULL)
     {
@@ -95,12 +133,55 @@ int main(int argc, char *argv[])
             tokens.push_back(line_tokens);
         }
     }
-    for(auto it : tokens){
-        for(auto jt : it){
-            std::cout << jt << " ";
-        }
-        std::cout << "\n";
+    if (!tokens.size())
+    {
+        fprintf(stderr,
+                "error: no token could be found\n");
+        exit(-1);
+    }
+
+    int res = handleORGKW(tokens[0]);
+    switch (res)
+    {
+    case -1:
+        fprintf(stderr,
+                "syntax error on first line\n");
+        exit(-1);
+        break;
+    case 1:
+        fprintf(stderr,
+                "syntax error: need ORG keyword on first line\n");
+        exit(-1);
+        break;
+    case 2:
+        fprintf(stderr,
+                "syntax error: need to start from 0x3000\n");
+        exit(-1);
+        break;
     }
     
+    uint16_t test = binToUI16("0b0001000000000000");
+    std::cout << test << std::endl;
+    // std::map<std::string, u_int16_t> M;
+    // M["ADD"] = 4096;   // 0b0001000000000000
+    // M["AND"] = 20480;  // 0b0101000000000000
+    // M["BR"] = 0;       // 0b0000000000000000
+    // M["JMP"] = 49152;  // 0b1100000000000000
+    // M["JSR"] = 16384;  // 0b0100000000000000
+    // M["JSRR"] = 16384; // 0b0100000000000000
+    // M["LD"] = 8192;    // 0b0010000000000000
+    // M["LDI"] = 20480 M["LDR"] =
+    //     M["LEA"] =
+    //         M["NOT"] =
+    //             M["RET"] =
+    //                 M["RTI"] =
+    //                     M["ST"] =
+    //                         M["STI"] =
+    //                             M["STR"] =
+    //                                 M["TRAP"] =
+    //                                     for (int i = 1; i < tokens.size(); ++i)
+    // {
+    // }
+
     return 0;
 }
